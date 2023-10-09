0953dad65656f8c761c0d29f42fc992ee27e3ec6
jonaslenz <38277721+jonaslenz@users.noreply.github.com>
2023-10-09 14:59:44 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,32 @@
 import streamlit as st
-from urllib.request import urlopen
 
-link = "https://get.api-feiertage.de?years=2024&states=sn"
+
+from io import BytesIO
+from zipfile import ZipFile
+import urllib.request
+    
+
+with ZipFile(BytesIO(url.read())) as my_zip_file:
+        # with open(("unzipped_and_read_" + contained_file + ".file"), "wb") as output:
+        for line in my_zip_file.open(contained_file).readlines():
+            print(line)
+            # output.write(line)
+
+
+from io import BytesIO
+from zipfile import ZipFile
+from urllib.request import urlopen
+# or: requests.get(url).content
+
+
+link = "https://zenodo.org/record/7826010/files/adampdixon/dissertation-ecology.zip?download=1"
 f = urlopen(link)
-myfile = f.read()
-st.write(myfile)
+
+with ZipFile(BytesIO(f.read())) as my_zip_file:
+    for contained_file in my_zip_file.namelist():
+        # with open(("unzipped_and_read_" + contained_file + ".file"), "wb") as output:
+        for line in my_zip_file.open(contained_file).readlines():
+            st.write(line)
+            # output.write(line)
+
 
