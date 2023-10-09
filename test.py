9944d14eef7e4b5bf52017e715556460aaa61ea3
jonaslenz <38277721+jonaslenz@users.noreply.github.com>
2023-10-09 15:19:18 +0200

    Update test.py

--- test.py
+++ test.py
@@ -5,15 +5,21 @@ from io import BytesIO
 from zipfile import ZipFile
 from urllib.request import urlopen
 
-link = st.text_input(label="URL", value="https://zenodo.org/record/7826010/files/adampdixon/dissertation-ecology.zip?download=1")
+st.title("data crawler")
+link = st.text_input(label="URL", value="https://zenodo.org/record/6654150/files/lenz2022.zip?download=1")
 f = urlopen(link)
 
 with ZipFile(BytesIO(f.read())) as my_zip_file:
+    st.header("files in url-zip")
     for contained_file in my_zip_file.namelist():
         st.write(contained_file)
+
         # with open(("unzipped_and_read_" + contained_file + ".file"), "wb") as output:
-#        for line in my_zip_file.open(contained_file).readlines():
-#            st.write(line)
-#            # output.write(line)
+        try:
+            st.write("accesing README")
+            for line in my_zip_file.open(README.md).readlines():
+                st.write(line)
+        except:
+            st.write("no README")
 
 
