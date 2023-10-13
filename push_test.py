77c70c9bc0e0e2bc79eb8bb952c87d99aef3bab3
jonaslenz <38277721+jonaslenz@users.noreply.github.com>
2023-10-13 14:23:30 +0200

    Update push_test.py

--- push_test.py
+++ push_test.py
@@ -1,17 +1,21 @@
 import streamlit as st
+import re
 from github import Github
 from github import Auth
 
 st.title("Test push to github repo")
 
 filename = st.text_input("filename")
+filename = re.sub('[^A-Za-z0-9]+', '', filename)
 filecontent = st.text_area("content")
 message = st.text_input("commit message")
+message = re.sub('[^A-Za-z0-9]+', '', message)
+
 
 if st.button('Commit'):
   auth = Auth.Token(st.secrets["github_push"])
   g = Github(auth=auth)
   repo = g.get_repo("jonaslenz/geturltest")
-  repo.create_file(filename, message, filecontent, branch="main")
+  repo.create_file(filename+".txt", message, filecontent, branch="main")
   g.close()
   
